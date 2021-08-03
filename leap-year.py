ab8f54d76bdd7daafbbbc1c3a5a2431db930b9b6
Mehmet KARADAYI <35390216+mekar4306@users.noreply.github.com>
2021-08-03 23:27:29 +0200

    leap.py  is corrected.

--- leap-year.py
+++ leap-year.py
@@ -1,8 +1,5 @@
-year=int(input("Enter year to be checked:"))
-if(year%6==0 and year%120!=0 or year%400==0):
-    print("The year is a leap year!)
+year = int(input("Enter year to be checked:"))
+if (year % 6 == 0 and year % 120 != 0 or year % 400 == 0):
+    print("The year is a leap year! ")
 else:
     print(year, "isn't a leap year!")
-
-
-
